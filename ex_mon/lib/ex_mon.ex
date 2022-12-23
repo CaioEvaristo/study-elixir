523808f030d06cbe39e4e21520d0dc10517bba10
Caioba <caio.evaristodesouza@hotmail.com>
2022-12-23 15:38:15 -0300

    i not remember, sorry guys

--- ex_mon/lib/ex_mon.ex
+++ ex_mon/lib/ex_mon.ex
@@ -5,28 +5,28 @@ defmodule ExMon do
   @computer_name "Robot"
 
   def create_player(name, move_avg, move_random, move_heal) do
-    Player.build(name, move_avg, move_random, move_heal)
+	Player.build(name, move_avg, move_random, move_heal)
   end
 
   def start_game(player) do 
-    @computer_name 
-    |> create_player(:punch, :kick, :heal) 
-    |> Game.start(player)
+	@computer_name 
+	|> create_player(:punch, :kick, :heal) 
+	|> Game.start(player)
 
-    Status.print_round_message()
+	Status.print_round_message()
   end
 
   def make_move(move) do
-    move
-    |> Actions.fetch_move()
-    |> do_move()
+	move
+	|> Actions.fetch_move()
+	|> do_move()
   end
 
   defp do_move({:error, move}), do: Status.print_wrong_move_message(move)
   defp do_move({:ok, move}) do
-    case move do
-      :move_heal -> "curando"
-      move -> Actions.attack(move)
-    end
+	case move do
+	  :move_heal -> "curando"
+	  move -> Actions.attack(move)
+	end
   end
 end
--- ex_mon/lib/game/status.ex
+++ ex_mon/lib/game/status.ex
@@ -10,4 +10,12 @@ defmodule ExMon.Game.Status do
     def print_wrong_move_message(move) do
         IO.puts("\n ===== Invalid move #{move} =====")
     end
+
+    def print_move_message(:computer, :attack, damage) do
+        IO.puts("\n ===== Tha player attack computer and damage is #{damage} =====")
+    end
+
+    def print_move_message(:player, :attack, damage) do
+        IO.puts("\n ===== Tha computer attack player and damage is #{damage} =====")
+    end
 end
