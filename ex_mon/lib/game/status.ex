6c7eec37fb5be7998b115b18d31804b4d616d052
Caioba <caio.evaristodesouza@hotmail.com>
2022-12-30 10:02:09 -0300

    create make move, damage and  message to  player

--- ex_mon/lib/game/status.ex
+++ ex_mon/lib/game/status.ex
@@ -1,12 +1,23 @@
 defmodule ExMon.Game.Status do
-    alias ExMon.Game
 
-    def print_round_message() do
+    def print_round_message(%{status: :started} = info) do
         IO.puts("\n ===== The game is started =====")
-        IO.inspect(Game.info())
+        IO.inspect(info)
         IO.puts("----------------------------------")
     end
-    
+
+    def print_round_message(%{status: :continue, turn: player} = info) do
+        IO.puts("\n ===== Its #{player} Turn =====")
+        IO.inspect(info)
+        IO.puts("----------------------------------")
+    end
+
+    def print_round_message(%{status: :game_over} = info) do
+        IO.puts("\n ===== Game Over =====")
+        IO.inspect(info)
+        IO.puts("----------------------------------")
+    end
+
     def print_wrong_move_message(move) do
         IO.puts("\n ===== Invalid move #{move} =====")
     end
@@ -17,5 +28,9 @@ defmodule ExMon.Game.Status do
 
     def print_move_message(:player, :attack, damage) do
         IO.puts("\n ===== Tha computer attack player and damage is #{damage} =====")
+    end
+
+    def print_move_message(player, :heal, life) do
+        IO.puts("\n ===== Tha #{player} heal #{life} of life =====")
     end
 end
