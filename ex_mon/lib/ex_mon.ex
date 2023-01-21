5fd01edefad149fa837cbd2e92b369c6ff4afc23
Caioba <caio.evaristodesouza@hotmail.com>
2023-01-20 21:29:40 -0300

    find

--- ex_mon/lib/ex_mon.ex
+++ ex_mon/lib/ex_mon.ex
@@ -3,14 +3,15 @@ defmodule ExMon do
   alias ExMon.Game.{Actions, Status}
 
   @computer_name "Robot"
+  @computer_move [:move_avg, :move_random, :move_heal]
 
   def create_player(name, move_avg, move_random, move_heal) do
 	  Player.build(name, move_avg, move_random, move_heal)
   end
 
-  def start_game(player) do 
-    @computer_name 
-    |> create_player(:punch, :kick, :heal) 
+  def start_game(player) do
+    @computer_name
+    |> create_player(:punch, :kick, :heal)
     |> Game.start(player)
 
     Status.print_round_message(Game.info())
@@ -20,7 +21,16 @@ defmodule ExMon do
     move
     |> Actions.fetch_move()
     |> do_move()
+
+    computer_move(Game.info())
   end
+
+  defp computer_move(%{turn: :computer, status: :continue}) do
+    move - {:ok, Enum.random(@computer_moves)}
+    do_move(move)
+  end
+
+  defp computer_move(), do: :ok
 
   defp do_move({:error, move}), do: Status.print_wrong_move_message(move)
   defp do_move({:ok, move}) do
